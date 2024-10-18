408631297c7fbb24507084130d94a2648f9d9788
Taufan Prasetyo <taufanremote@gmail.com>
2024-10-18 10:34:26 +0700

    Streaming example2

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,12 @@
-import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons'
+import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons'
 import { Suspense } from 'react'
-import { fetchCardData } from '../../lib/data'
-import { Card } from '../../ui/dashboard/cards'
+import CardWrapper from '../../ui/dashboard/cards'
 import LatestInvoices from '../../ui/dashboard/latest-invoices'
 import RevenueChart from '../../ui/dashboard/revenue-chart'
 import { lusitana } from '../../ui/fonts'
 
 async function Dashboard() {
-    const { totalPaidInvoices, totalPendingInvoices, numberOfCustomers, numberOfInvoices, } = await fetchCardData()
+
 
     return (
         <main>
@@ -16,26 +15,9 @@ async function Dashboard() {
             </h1>
 
             <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-                <Card
-                    title='Collected'
-                    value={totalPaidInvoices}
-                    type='collected'
-                />
-                <Card
-                    title='Pending'
-                    value={totalPendingInvoices}
-                    type='pending'
-                />
-                <Card
-                    title='Total Invoices'
-                    value={numberOfInvoices}
-                    type='invoices'
-                />
-                <Card
-                    title='Total Customers'
-                    value={numberOfCustomers}
-                    type='customers'
-                />
+                <Suspense fallback={<CardSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
             </div>
 
             <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
