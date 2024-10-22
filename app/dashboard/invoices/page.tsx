c943592299d3b02e0201da91182052f406c052be
Taufan Prasetyo <taufanremote@gmail.com>
2024-10-22 08:32:39 +0700

    Pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,7 @@
+import { fetchInvoicesPages } from '@/app/lib/data'
 import { lusitana } from '@/app/ui/fonts'
 import { CreateInvoice } from '@/app/ui/invoices/buttons'
+import Pagination from '@/app/ui/invoices/pagination'
 import InvoicesTable from '@/app/ui/invoices/table'
 import Search from '@/app/ui/search'
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons'
@@ -15,6 +17,8 @@ interface InvoicePageProps {
 async function InvoicePage({ searchParams }: InvoicePageProps) {
     const query = searchParams?.query || ''
     const currentPage = Number(searchParams?.page) || 1
+
+    const totalPages = await fetchInvoicesPages(query)
 
     return (
         <div className='w-full'>
@@ -32,7 +36,7 @@ async function InvoicePage({ searchParams }: InvoicePageProps) {
             </Suspense>
 
             <div className='mt-5 flex w-full justify-center'>
-
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     )
