781053e7d28f047e2bd315d0906cdb8f095653d7
Taufan Prasetyo <taufanremote@gmail.com>
2024-10-27 07:18:19 +0700

    Update & Delete Invoices

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
+import EditInvoiceForm from '@/app/ui/invoices/edit-form'
+import React from 'react'
+
+const breadcrumbs = [
+    { label: 'Invoices', href: '/invoices' },
+    { label: 'Create Invoice', href: '/invoices/create', active: true },
+]
+
+async function EditInvoicePage(props: { params: Promise<{ id: string }> }) {
+    const params = await props.params
+    const id = params.id
+
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers()
+    ])
+
+    return (
+        <main>
+            <Breadcrumbs breadcrumbs={breadcrumbs} />
+
+            <EditInvoiceForm invoice={invoice} customers={customers} />
+        </main>
+    )
+}
+
+export default EditInvoicePage
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const FormSchema = z.object({
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -32,4 +33,33 @@ export async function createInvoice(formData: FormData) {
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    })
+
+    const amountInCents = amount * 100
+    const date = new Date().toISOString().split('T')[0]
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}, date = ${date}
+        WHERE id = ${id}
+    `
+
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`
+        DELETE FROM invoices
+        WHERE id = ${id}
+    `
+
+    revalidatePath('/dashboard/invoices')
 }
