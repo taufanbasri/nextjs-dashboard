1605b7cd3e1c380f97c905de3e753572006aa71b
Taufan Prasetyo <taufanremote@gmail.com>
2024-10-27 13:57:27 +0700

    Error Handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import EditInvoiceForm from '@/app/ui/invoices/edit-form'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 const breadcrumbs = [
@@ -16,6 +17,10 @@ async function EditInvoicePage(props: { params: Promise<{ id: string }> }) {
         fetchInvoiceById(id),
         fetchCustomers()
     ])
+
+    if (!invoice) {
+        notFound()
+    }
 
     return (
         <main>
